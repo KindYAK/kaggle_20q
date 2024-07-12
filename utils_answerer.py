d2f2ed188dd8fbddb8dd30becb32d5b07ceb43ea
Kirill Yakunin <yakunin.k@mail.ru>
2024-07-12 14:08:10 +0500

    guesser, answerer - double prompt

--- utils_answerer.py
+++ utils_answerer.py
@@ -1,11 +1,41 @@
 from utils import generate_answer, SYSTEM_PROMPT_ANSWERER, get_qa_history_prompt
 
+def _reason_answer(
+    obs,
+    model,
+    tokenizer,
+    id_eot,
+    max_new_tokens_reason: int = 300,
+):
+    sys_prompt = SYSTEM_PROMPT_ANSWERER
+    user_prompt = f"""You are playing "20 Questions" game.
+
+{get_qa_history_prompt(obs, include_guesses=True)}
+
+The guessed keyword is "{obs.keyword}".
+
+Another player is asking: "{obs.questions[-1]}".
+
+You goal is to reason about how to answer this question about the keyword as truthfully as possible.
+The answer can only be YES or NO. Nothing else. Even if you don't know, even if there's no simple yes/no answer - the answer still HAAS TO be YES or NO.
+Given this harsh constraint, still do your best to be useful, truthful, and helpful to the other player.
+
+Format your answer like this: reason about how to answer for 2-3 sentences. If the answer is obvious - state it. If not - reason about whether YES or NO would be more helpful and truthful.
+"""
+
+    chat_template = f"""system\n\n{sys_prompt}"""
+    chat_template += "user\n\n"
+    chat_template += f"{user_prompt}"
+
+    output = generate_answer(chat_template, model, tokenizer, id_eot, max_new_tokens=max_new_tokens_reason)
+    return output
 
 def answer(
     obs,
     model,
     tokenizer,
     id_eot,
+    max_new_tokens_reason: int = 300,
 ):
     sys_prompt = SYSTEM_PROMPT_ANSWERER
 #     user_prompt = f"""You are playing "20 Questions" game.
@@ -26,6 +56,8 @@ def answer(
 # Don't output anything else.
 # """
 
+    reasoning = _reason_answer(obs, model, tokenizer, id_eot, max_new_tokens_reason)
+    print("!!!", reasoning)  # TODO DEBUG
     user_prompt = f"""You are playing "20 Questions" game.
 
 {get_qa_history_prompt(obs, include_guesses=True)}
@@ -37,6 +69,8 @@ Another player is asking: "{obs.questions[-1]}".
 You goal is to answer this question about the keyword as truthfully as possible.
 You can only answer YES or NO. Nothing else. Even if you don't know, even if there's no simple yes/no answer - you still HAVE TO answer YES or NO.
 Given this harsh constraint, still do your best to be useful, truthful, and helpful to the other player.
+
+Here are some considerations for answering the questions: {reasoning}
 
 Format your answer like this: Your last and only line should be the answer, only YES or NO.
 Don't output anything else.
@@ -54,5 +88,7 @@ Don't output anything else.
     try:
         assert answer in ["yes", "no"], f"Invalid answer: {answer}"
     except:
+        print("!!!", answer)
+        print(reasoning)
         return "no"
     return answer
--- utils_guesser.py
+++ utils_guesser.py
@@ -1,36 +1,44 @@
 from utils import generate_answer, SYSTEM_PROMPT_ASKER, get_qa_history_prompt
 
 
+def _reason(
+    obs,
+    model,
+    tokenizer,
+    id_eot,
+    max_new_tokens_reason: int = 300,
+):
+    sys_prompt = SYSTEM_PROMPT_ASKER
+    user_prompt = f"""You are playing "20 Questions" game.
+
+{get_qa_history_prompt(obs, include_guesses=True)}
+
+You goal is to make a guess about the keyword given the history of questions and answers.
+History above contains previous guesses. When the guess is correct, the game stops. So, there's not reason to try a guess that was already made!
+
+You need to reason about what you know and what conclusions can you make before making a guess.
+Format your answer like this: reason about your decision for 2-3 sentences. Summarize what you know, summarize space of possibilities, etc.
+    """
+
+    chat_template = f"""<|begin_of_text|><|start_header_id|>system<|end_header_id|>\n\n{sys_prompt}<|eot_id|>"""
+    chat_template += "<|start_header_id|>user<|end_header_id|>\n\n"
+    chat_template += f"{user_prompt}<|eot_id|>"
+
+    output = generate_answer(chat_template, model, tokenizer, id_eot, max_new_tokens=max_new_tokens_reason)
+    return output
+
+
 def guess(
     obs,
     model,
     tokenizer,
     id_eot,
+    max_new_tokens_reason: int = 300,
 ):
     sys_prompt = SYSTEM_PROMPT_ASKER
-#     user_prompt = f"""You are playing "20 Questions" game.
-#
-# {get_qa_history_prompt(obs, include_guesses=True)}
-#
-# You goal is to make a guess about the keyword given the history of questions and answers.
-# History above contains previous guesses. When the guess is correct, the game stops. So, there's not reason to try a guess that was already made!
-# Also, keep in mind that guess only counts if it's correct letter-by-letter. So, if you have strong reasons to believe that one of the previous guesses is correct, but was
-# phrased differently - you can try to guess it again.
-# Don't forget that some inconsistencies in answers can be caused by errors in the answering LLM.
-# The keyword can consist of one or more words.
-#
-# Don't forget, that you should never guess whatever was already guessed before! You should always try new options to gain more information!
-# Only output the same guess if there might be another accepted phrasing of the same keyword (which is usually the case when there are several words).
-#
-# Format your answer like this: First, reason about your decision for 1-2 sentences. Summarize what you know, summarize space of possibilities, etc.
-# Never reason for more than 4 sentences! After 4 sentences just get to the answer (guess for keyword) on a new line!
-# Then, your last line should be the keyword that you want to guess.
-# Don't output anything else. Only output the keyword that you want to guess. Don't say anything like "My guess is {{keyword}}" - always just "{{keyword}}"
-# Example output:
-# Based on the questions I know that keyword is ..., ..., .... Hence, it's possible that keyword could be .... My best guess would be {{keyword}}
-# {{keyword}}
-# """
 
+    reasoning = _reason(obs, model, tokenizer, id_eot, max_new_tokens_reason)
+    print("!!!", reasoning) # TODO DEBUG
     user_prompt = f"""You are playing "20 Questions" game.
 
 {get_qa_history_prompt(obs, include_guesses=True)}
@@ -44,6 +52,8 @@ The keyword can consist of one or more words.
 
 Don't forget, that you should never guess whatever was already guessed before! You should always try new options to gain more information!
 Only output the same guess if there might be another accepted phrasing of the same keyword (which is usually the case when there are several words).
+
+Here are some considerations about what we already know: {reasoning}
 
 Format your answer like this: only output the keyword that you want to guess, nothing else. Don't say anything like "My guess is {{keyword}}" - always just "{{keyword}}"
 """
