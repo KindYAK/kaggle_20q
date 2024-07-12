11f0dce03d1b8ddc73d1d925bcff86341118b958
Kirill Yakunin <yakunin.k@mail.ru>
2024-07-12 21:10:33 +0500

    simplify prompts :(

--- utils_asker.py
+++ utils_asker.py
@@ -1,51 +1,26 @@
 from utils import generate_answer, SYSTEM_PROMPT_ASKER, get_qa_history_prompt
 
 
-def generate_candidates(obs, model, tokenizer, id_eot, candidates_to_generate=5, max_new_tokens_reason: int = 500):
-    sys_prompt = SYSTEM_PROMPT_ASKER
-    user_prompt = f"""You are playing "20 Questions" game.
-    
-{get_qa_history_prompt(obs)}
-
-You need to generate {candidates_to_generate} best candidates for the next question to ask.
-You should mix safe questions with more creative and non-orthodox ones.
-Safe questions are usually ones that split the space of possibilities in half.
-If you have a few questions left, you can start acting more aggressively, intuitively checking some less probable, but high-reward hypotheses.
-Consider asking non-standard questions like "Does the keyword consist of a single word?, however only do it if you don't know it yet, and if you think that it's important to know at this point in the game.
-Consider asking questions like "Does the keyword start with letter 'A'?" or "Does the keyword contain letter 'E'?". However, don't overdo it, focus on more substantial questions.
-
-Format your answer like this: output every question on a new line.
-Then, reason for 2-3 sentences about which question is the best one, given the question-answer history.
-"""
-
-    chat_template = f"""<|begin_of_text|><|start_header_id|>system<|end_header_id|>\n\n{sys_prompt}<|eot_id|>"""
-    chat_template += "<|start_header_id|>user<|end_header_id|>\n\n"
-    chat_template += f"{user_prompt}<|eot_id|>"
-    output = generate_answer(chat_template, model, tokenizer, id_eot, max_new_tokens=max_new_tokens_reason, system_prompt=sys_prompt)
-    return output.strip().split("\n")
-
-
 def ask(
     obs,
     model,
     tokenizer,
     id_eot,
-    max_new_tokens_reason: int = 500,
 ):
-    candidates = generate_candidates(obs, model, tokenizer, id_eot, max_new_tokens_reason=max_new_tokens_reason)
-    candidates_message = "\n".join(candidates)
-    print("!", candidates_message) # TODO DEBUG
     sys_prompt = SYSTEM_PROMPT_ASKER
     user_prompt = f"""You are playing "20 Questions" game.
     
 {get_qa_history_prompt(obs)}
     
-Here are some candidates for the next question and some considerations for choosing the best one:
-{candidates_message}
-
-You goal is to pick the best next question.
+You goal is to choose the best next question.
 Safe questions are usually ones that split the space of possibilities in half given the previous answers.
 If you have a few questions left, you can start acting more aggressively, intuitively checking some less probable, but high-reward hypotheses.
+
+You should keep balance of safe questions and more creative and non-orthodox ones.
+Safe questions are usually ones that split the space of possibilities in half.
+If you have a few questions left, you can start acting more aggressively, intuitively checking some less probable, but high-reward hypotheses.
+Consider asking non-standard questions like "Does the keyword consist of a single word?, however only do it if you don't know it yet, and if you think that it's important to know at this point in the game.
+Consider asking questions like "Does the keyword start with letter 'A'?" or "Does the keyword contain letter 'E'?". However, don't overdo it, focus on more substantial questions.
 
 In any case, try to make reason about what decision is the best here, balance risk, and choose the best next question. Act as a competetive extremely smart player that you are.
 
--- utils_guesser.py
+++ utils_guesser.py
@@ -1,44 +1,14 @@
 from utils import generate_answer, SYSTEM_PROMPT_ASKER, get_qa_history_prompt
 
 
-def _reason(
-    obs,
-    model,
-    tokenizer,
-    id_eot,
-    max_new_tokens_reason: int = 300,
-):
-    sys_prompt = SYSTEM_PROMPT_ASKER
-    user_prompt = f"""You are playing "20 Questions" game.
-
-{get_qa_history_prompt(obs, include_guesses=True)}
-
-You goal is to make a guess about the keyword given the history of questions and answers.
-History above contains previous guesses. When the guess is correct, the game stops. So, there's not reason to try a guess that was already made!
-
-You need to reason about what you know and what conclusions can you make before making a guess.
-Format your answer like this: reason about your decision for 2-3 sentences. Summarize what you know, summarize space of possibilities, etc.
-    """
-
-    chat_template = f"""<|begin_of_text|><|start_header_id|>system<|end_header_id|>\n\n{sys_prompt}<|eot_id|>"""
-    chat_template += "<|start_header_id|>user<|end_header_id|>\n\n"
-    chat_template += f"{user_prompt}<|eot_id|>"
-
-    output = generate_answer(chat_template, model, tokenizer, id_eot, max_new_tokens=max_new_tokens_reason, system_prompt=sys_prompt)
-    return output
-
-
 def guess(
     obs,
     model,
     tokenizer,
     id_eot,
-    max_new_tokens_reason: int = 300,
 ):
     sys_prompt = SYSTEM_PROMPT_ASKER
 
-    reasoning = _reason(obs, model, tokenizer, id_eot, max_new_tokens_reason)
-    print("!!!", reasoning) # TODO DEBUG
     user_prompt = f"""You are playing "20 Questions" game.
 
 {get_qa_history_prompt(obs, include_guesses=True)}
@@ -52,8 +22,6 @@ The keyword can consist of one or more words.
 
 Don't forget, that you should never guess whatever was already guessed before! You should always try new options to gain more information!
 Only output the same guess if there might be another accepted phrasing of the same keyword (which is usually the case when there are several words).
-
-Here are some considerations about what we already know: {reasoning}
 
 Format your answer like this: only output the keyword that you want to guess, nothing else. Don't say anything like "My guess is {{keyword}}" - always just "{{keyword}}"
 """
